6fe15765e476820d4f55ffccad84fd56c13afdf0
tysbrown <tylershaynebrown@gmail.com>
2021-06-09 12:56:46 -0400

    Completed desktop build of homepage
    * Built the about section.
    
    * Built Card component, as well as both carousels.
    
    * Completed scaffold of NavBar on desktop and mobile.

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -3,6 +3,9 @@ import Header from "./header";
 import NavBar from "./navBar";
 import "../styles/layout.css";
 import * as styles from "./layout.module.css";
+import { StaticImage } from "gatsby-plugin-image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Layout = ({ children }) => (
   <>
@@ -10,10 +13,66 @@ const Layout = ({ children }) => (
     <NavBar />
     <div>{children}</div>
     <footer className={styles.footer}>
-      <div className={styles.footerWrapper}>
-        <div className={styles.siteInfo}>
-          COPYRIGHT &copy; {new Date().getFullYear()} ORANGE BLOSSOM CANDIES
-        </div>
+      <div>
+        <StaticImage
+          src="../assets/roundlogo.png"
+          width={200}
+          height={200}
+          className={styles.logo}
+        />
+      </div>
+      <div>
+        <h3>About</h3>
+        <p>
+          Orange Blossom Candies & Cream is a mother and daughter owned ice
+          cream parlor and candy shop located in Montverde just beside Montverde
+          Academy.
+        </p>
+      </div>
+      <div>
+        <h3>Address</h3>
+        <p>
+          <span>16937 Lakeside Dr</span>
+          <span>Montverde, FL</span>
+          <span>34756</span>
+        </p>
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <p>
+          <span>Orangeblossomcandy@gmail.com</span>
+          <span>TEL: (352) 531-1139</span>
+          <span>
+            <FontAwesomeIcon
+              icon={faFacebook}
+              size="2x"
+              className={styles.icon}
+            />
+            <FontAwesomeIcon
+              icon={faInstagram}
+              size="2x"
+              className={styles.icon}
+            />
+          </span>
+        </p>
+      </div>
+      <div>
+        <h3>Hours</h3>
+        <p>
+          <span>Monday 12-8PM</span>
+          <span>Tuesday 12-8PM</span>
+          <span>Wednesday 12-8PM</span>
+          <span>Thursday 12-6PM</span>
+          <span>FRI-SAT CLOSED</span>
+          <span>Sunday 12-6PM</span>
+        </p>
+      </div>
+      <div>
+        <h3>Site Map</h3>
+        <span>Home</span>
+        <span>About</span>
+        <span>Menu</span>
+        <span>Contact</span>
       </div>
     </footer>
   </>
